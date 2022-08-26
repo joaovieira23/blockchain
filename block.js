14baa1ac678c1662744d31902a2d14baee643f1d
João Andrade <joaovictorvieira.23@hotmail.com>
2022-08-26 16:41:24 -0300

    feat: create the hash through SHA256

--- block.js
+++ block.js
@@ -1,3 +1,5 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block {
 
     constructor (timestamp, lastHash, hash, data) {
@@ -22,10 +24,16 @@ class Block {
     static mineBlock(lastBlock, data) {
         const timestamp = Date.now();
         const lastHash = lastBlock.hash;
-        const hash = 'a-fazer-hash';
+        const hash = Block.hash(timestamp, lastHash, data);
 
         return new this(timestamp, lastHash, hash, data);
     }
+
+    static hash(timestamp, lastHash, data) {
+        return SHA256(`${timestamp}${lastHash}${data}`).toString();
+    };
+
+    //SHA - 256
 }
 
 module.exports = Block;
