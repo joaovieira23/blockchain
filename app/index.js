9a9b85e7b934650eb475b2525dbc1c81b906fef5
João Andrade <joaovictorvieira.23@hotmail.com>
2022-08-29 15:10:15 -0300

    feat: create api get request

--- app/index.js
+++ app/index.js
@@ -5,4 +5,10 @@ const HTTP_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 const bc = new Blockchain();
 
+app.get('/blocks', (req, res) => {
+    res.json(bc.chain);
+});
+
+app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
+
 // $ HTTP_PORT = 3002 npm run dev
