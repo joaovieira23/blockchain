2823539348dbfb8ded680dafeecc7b1b86172379
João Andrade <joaovictorvieira.23@hotmail.com>
2022-08-29 12:40:51 -0300

    test: chains validation test

--- blockchain.test.js
+++ blockchain.test.js
@@ -3,9 +3,11 @@ const Block = require('./block');
 
 describe('Blockchain', () => {
     let bc;
+    let bc2;
 
     beforeEach(() => {
         bc = new Blockchain;
+        bc2 = new Blockchain;
     });
 
     it('Starts with genesis block', () => {
@@ -18,4 +20,23 @@ describe('Blockchain', () => {
 
         expect(bc.chain[bc.chain.length - 1].data).toEqual(data);
     });
+
+    it('Validates a valid chain', () => {
+        bc2.addBlock('500U$');
+
+        expect(bc.isValidChain(bc2.chain)).toBe(true);
+    });
+
+    it('Invalidates a chain with a corrupt genesis block', () => {
+        bc2.chain[0].data = '0U$';
+
+        expect(bc.isValidChain(bc2.chain)).toBe(false);
+    });
+
+    it('Invalidates a corrupt chain', () => {
+        bc2.addBlock('200$');
+        bc2.chain[1].data = '0U$';
+
+        expect(bc.isValidChain(bc2.chain)).toBe(false);
+    })
 });
