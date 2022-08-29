e609b59df23dbacbee30964cb721099585cf271c
João Andrade <joaovictorvieira.23@hotmail.com>
2022-08-29 13:54:39 -0300

    chore: starting to configure api

---
+++ app/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const Blockchain = require('../blockchain');
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
+
+const app = express();
+const bc = new Blockchain();
+
+// $ HTTP_PORT = 3002 npm run dev
