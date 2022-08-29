f9c56077840f29b20e7aeaae2ddf1396d123a590
João Andrade <joaovictorvieira.23@hotmail.com>
2022-08-29 15:35:01 -0300

    feat: add blocks to blockchain with post request

--- app/index.js
+++ app/index.js
@@ -5,8 +5,17 @@ const HTTP_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 const bc = new Blockchain();
 
+app.use(express.json());
+
 app.get('/blocks', (req, res) => {
     res.json(bc.chain);
+});
+
+app.post('/mine', (req, res) => {
+    const block = bc.addBlock(req.body.data);
+    console.log(`New Block added: ${block.toString()}`);
+
+    res.redirect('/blocks');
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
